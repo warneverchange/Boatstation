132a9f09694d4fe56153a925a3ab472f08a918ba
warneverchange <ceckinkonstantin@gmail.com>
2023-04-05 20:12:57 +0500

    Init react + data grip project.

---
+++ src/main/java/com/portfolio/boatstation/BoatStationApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.boatstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoatStationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/boatstation/BoatStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.boatstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoatStationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
