2104b7721e7ff4c199e3a20fd404e426a2973e71
warneverchange <ceckinkonstantin@gmail.com>
2023-04-05 20:15:46 +0500

    Init react

---
+++ src/main/webapp/boatstationjs/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "bootstrap";
+import { Component } from "react";
+import { Navbar } from "react-bootstrap";
+import "react-bootstrap/Button"
+
+class App extends Component {
+    render() {
+        return new Button();
+    }
+}
+
+export default App;
